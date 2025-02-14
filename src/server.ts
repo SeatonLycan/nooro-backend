dfb3d206ea5cffad0b604a5cc95324d939a67307
Seaton Lycan <slycan42@gmail.com>
2025-02-14 13:46:50 -0700

    add cors to fix issues fetching data from next app

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import router from './routes/index';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from Next.js
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use('/api', router);
 
 app.listen(PORT, () => {
