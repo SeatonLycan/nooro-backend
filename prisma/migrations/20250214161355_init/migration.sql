ae7c75a73e6606b63c4e7ccceac9183823d880a8
Seaton Lycan <slycan42@gmail.com>
2025-02-14 09:52:53 -0700

    Create Prisma schema and generate Task Table

---
+++ prisma/migrations/20250214161355_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `color` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
